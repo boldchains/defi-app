ab2f43dbd6e3261d4180f6becbbeda08e3b44d06
boldchain <jameslin848@gmail.com>
2021-09-20 09:11:42 -0400

    Implemented injected connector through MetaMask

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,39 @@
 import { Button, Typography, Box } from '@mui/material';
-import { grey, indigo } from '@mui/material/colors';
+import { indigo } from '@mui/material/colors';
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
 import React from 'react';
+import { injected, ledger } from '../../connectors';
+import { Account, Balance } from '..';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { LedgerConnector } from '@web3-react/ledger-connector';
+
+enum ConnectorNames {
+  Injected = 'Injected',
+  Ledger = 'Ledger',
+}
+
+const connectorsByName: {
+  [connectorName in ConnectorNames]: InjectedConnector | LedgerConnector;
+} = {
+  [ConnectorNames.Injected]: injected,
+  [ConnectorNames.Ledger]: ledger,
+};
 
 const Navbar = () => {
+  const { activate, active } = useWeb3React();
+
+  const connectWallet = (con = connectorsByName['Injected']) => {
+    try {
+      activate(con, undefined, true).catch((error) => {
+        if (error instanceof UnsupportedChainIdError) {
+          activate(con);
+        }
+      });
+    } catch (error) {
+      alert('Failed to connect.');
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -15,17 +46,25 @@ const Navbar = () => {
       <Typography variant="h6" component="h6">
         Defi App
       </Typography>
-      <Button
-        color="primary"
-        variant="outlined"
-        sx={{
-          bgcolor: 'white',
-          '&:hover': {
+      {active ? (
+        <Box display="flex" flexDirection="column" textAlign="end">
+          <Account />
+          <Balance />
+        </Box>
+      ) : (
+        <Button
+          color="primary"
+          variant="outlined"
+          sx={{
             bgcolor: 'white',
-          },
-        }}>
-        Connect Wallet
-      </Button>
+            '&:hover': {
+              bgcolor: 'white',
+            },
+          }}
+          onClick={() => connectWallet()}>
+          Connect Wallet
+        </Button>
+      )}
     </Box>
   );
 };
