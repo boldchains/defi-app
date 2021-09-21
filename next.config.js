a973575659ae19d5e11718cd96df22f5a98692e1
boldchain <jameslin848@gmail.com>
2021-09-20 21:55:17 -0400

    Update env variable names & added etherscan url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    infuraId: '8d810610fe7741cc9753cbaafb1f000c',
-    erc20_address: '0xaD6D458402F60fD3Bd25163575031ACDce07538D',
+    ETHERSCAN_URL: 'https://etherscan.io/',
+    RPC_URL_3: 'https://ropsten.infura.io/v3/8d810610fe7741cc9753cbaafb1f000c',
+    ERC20_ADDRESS: '0xaD6D458402F60fD3Bd25163575031ACDce07538D',
   },
 };
