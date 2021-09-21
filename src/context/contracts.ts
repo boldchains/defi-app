19a28399e8df1b55f78f565ee13e7dfaa8cded62
boldchain <jameslin848@gmail.com>
2021-09-20 20:22:29 -0400

    Implemented basic transfer the logic

---
+++ src/context/contracts.ts
@@ -0,0 +1,12 @@
+import { Contract } from '@ethersproject/contracts';
+import { Web3Provider } from '@ethersproject/providers';
+import { daiAbi } from './abi/dai';
+
+const ERC20_ADDR = process.env.erc20_address || '';
+
+/**
+ * ERC20 Contract instance
+ */
+export function getERC20Contract(library?: Web3Provider) {
+  return new Contract(ERC20_ADDR, daiAbi, library?.getSigner());
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,84 @@
-import { Button, Container, FormControl, TextField, Box } from '@mui/material';
 import type { NextPage } from 'next';
+import React, { useEffect, useState } from 'react';
+import { Button, Container, TextField, Box } from '@mui/material';
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React } from '@web3-react/core';
+import { getERC20Contract } from '../context/contracts';
+import { formatEther, parseEther } from '@ethersproject/units';
+import { ErrorCode } from '@ethersproject/logger';
 
 const Home: NextPage = () => {
+  const trasferFormRef = React.useRef<HTMLFormElement>(null);
+  const { library, account } = useWeb3React<Web3Provider>();
+  const cont = getERC20Contract(library);
+  const [daiBalance, setDaiBalance] = useState<string>('');
+  const [amount, setAmount] = useState<string>();
+  const [accountTo, setAccountTo] = useState<string>();
+  const [submitted, setSubmitted] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (library) {
+      cont.balanceOf(account).then((bal: any) => setDaiBalance(bal));
+    }
+  }, [library, account]);
+
+  const handleSubmitTransfer = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    if (!amount || !accountTo) return;
+
+    cont
+      .transfer(accountTo, parseEther(amount))
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      })
+      .finally(() => {
+        setSubmitted(false);
+        trasferFormRef.current?.reset();
+      });
+
+    setSubmitted(true);
+  };
+
   return (
     <Container maxWidth="xs">
-      <form>
+      <form
+        ref={trasferFormRef}
+        onSubmit={handleSubmitTransfer}
+        noValidate
+        autoComplete="off">
         <Box>
-          <FormControl fullWidth>
-            <TextField
-              id="dai-amount"
-              label="Enter DAI Amount"
-              helperText={`Balance: ${'50.69'} DAI`}
-              variant="filled"
-              fullWidth
-            />
-          </FormControl>
+          <TextField
+            id="dai-amount"
+            label="Enter DAI Amount"
+            helperText={`Balance: ${daiBalance && formatEther(daiBalance)} DAI`}
+            variant="filled"
+            fullWidth
+            inputProps={{
+              pattern: /([0-9]*[.])?[0-9]+/,
+            }}
+            onChange={(e) => setAmount(e.target.value)}
+          />
         </Box>
         <Box mt={2}>
-          <FormControl fullWidth>
-            <TextField
-              id="recipients-address"
-              label="Enter recipients address"
-              variant="filled"
-              fullWidth
-            />
-          </FormControl>
+          <TextField
+            id="recipients-address"
+            label="Enter recipients address"
+            variant="filled"
+            fullWidth
+            onChange={(e) => setAccountTo(e.target.value)}
+          />
         </Box>
         <Box mt={4} paddingX={4}>
-          <Button variant="contained" color="primary" fullWidth>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            disabled={submitted}>
             Send
           </Button>
         </Box>
