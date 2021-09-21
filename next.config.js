acc42ad6b3e42b8d4a3e425b598a3737392d0edb
boldchain <jameslin848@gmail.com>
2021-09-20 20:17:23 -0400

    Added chain provider environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    infuraId: '8d810610fe7741cc9753cbaafb1f000c',
+    erc20_address: '0xaD6D458402F60fD3Bd25163575031ACDce07538D',
+  },
+};
